e63295ddff385cc191709a5b93656699709f57fd
phenybutum <phenybutum@gmail.com>
2021-05-15 15:17:35 +0300

    [NEAP-1] updated pubspec, added some style files

--- newsapp/lib/presentation/app.dart
+++ newsapp/lib/presentation/app.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:newsapp/presentation/scenes/main_screen/main_screen.dart';
+import 'package:newsapp/presentation/styles/design_config.dart';
 
 class App extends StatelessWidget {
-
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'NewsApp',
-      home: Builder(builder: (context) => MainScreen()),
+    return ScreenUtilInit(
+      designSize: DesignConfig.size,
+      allowFontScaling: true,
+      builder: () => MaterialApp(
+        title: 'NewsApp',
+        home: Builder(builder: (context) => MainScreen()),
+      ),
     );
   }
 }
---
+++ newsapp/lib/presentation/styles/design_config.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+class DesignConfig {
+  static final size = Size(414, 736);
+}
